03122a11281c28430dc3b5d489d35e97598d158e
Nurmuhammad <nursherzil@gmail.com>
2026-04-03 23:02:42 +0500

    sleep

--- Day5/main.c
+++ Day5/main.c
@@ -2,11 +2,15 @@
 #include <string.h>
 
 int main(void) {
-    char mystr[6] = "Hello";
+    char mystr[] = "Hello";
     char dest[50];
-    strcpy(dest, mystr);
+    strcpy(dest, mystr); // dest degan variablega mystr ni copy qiladi
     printf("%s\n", mystr); // shunckai print qilish
     printf("%lu\n", strlen(mystr)); // string sizeni korsatadi output 5
-    printf("%s\n", dest);
+    printf("%s\n", dest); 
+
+    char world[] = " World";
+    strcat(mystr, world);
+    printf("%s", mystr);
     return 0;
 }
