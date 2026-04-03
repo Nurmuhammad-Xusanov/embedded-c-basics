8d4cda4fc2394ddc30a7bd294e4693dd4f8dda57
Nurmuhammad <nursherzil@gmail.com>
2026-04-03 22:49:36 +0500

    fix issues

--- Day5/main.c
+++ Day5/main.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 int main(void) {
-    char mystr[5] = "Hello";
-    char dest[10];
+    char mystr[6] = "Hello";
+    char dest[50];
     strcpy(dest, mystr);
     printf("%s\n", mystr); // shunckai print qilish
     printf("%lu\n", strlen(mystr)); // string sizeni korsatadi output 5
-    // printf("%s\n", dest);
+    printf("%s\n", dest);
     return 0;
 }
