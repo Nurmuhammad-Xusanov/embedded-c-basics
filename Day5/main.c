5a86179cc1cf4788d60d6b7c89525d9c0b446abb
Nurmuhammad <nursherzil@gmail.com>
2026-04-05 01:00:20 +0500

    strcmp

--- Day5/main.c
+++ Day5/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 
 int main(void) {
     char mystr[] = "Hello";
@@ -11,6 +12,23 @@ int main(void) {
 
     char world[] = " World";
     strcat(mystr, world);
-    printf("%s", mystr);
+    printf("%s\n", mystr);
+
+    char s1[] = "apple";
+    char s2[] = "apple";
+    char s3[] = "banana";
+    char s4[] = "Apple";
+    // if (strcmp(s1, s2) == 0) {
+    //     printf("s1 va s2 teng\n");
+    // }
+
+    // if (strcmp(s1, s3) < 0) {
+    //     printf("s3 s1 dan kichik");
+    // }
+    printf("%d\n", strcmp(s1, s2)); // 0 chiqadi chunki teng
+    printf("%d\n", strcmp(s1, s3)); // -1 chiqadi cuz s1 kichik s3 dan
+    printf("%d\n", strcmp(s3,s1)); // 1 chiqadi cuz s3 katta s1 dan
+    printf("%d\n", strcmp(s3, s4)); // 32 chiqadi cuz orasida 32 ta harf bor
+    printf("%d\n", strcasecmp(s1, s4)); // 0 chiqadi cuz lowercase
     return 0;
 }
