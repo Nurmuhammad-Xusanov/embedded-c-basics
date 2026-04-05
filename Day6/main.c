0abceb9a3ea662010131462946efbdfd1096de1b
Nurmuhammad <nursherzil@gmail.com>
2026-04-05 18:44:22 +0500

    bitwise

--- Day6/main.c
+++ Day6/main.c
@@ -15,11 +15,22 @@ int main(void) {
     }
 
     int a = 10, b = 4, c = 10;
-    printf("%d\n", a > b && a == c);
-    printf("%d\n", a < b || a == c);
-    printf("%d\n", !(a == c));
+    printf("%d\n", a > b && a == c); // 1 ikalasi true
+    printf("%d\n", a < b || a == c); // 1 bittasi true
+    printf("%d\n", !(a == c)); // 0 trueni teskarisi
 
 
+    // BITWISE
 
+    int x = 5; // 00000101 binary
+    int result = x & 1; // agar toq son bolsa 1 qaytaradi bolmasa 0
+    printf("%d\n", result);
+
+    if (x & 1) {
+        printf("Toq son: %d\n", x);
+    } else {
+        printf("Juft son: %d\n", x);
+       
+    }
     return 0;
 }
