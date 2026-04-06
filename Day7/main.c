78d7d2eaf416ab92db1cc665d0dffcf433f243bf
Nurmuhammad <nursherzil@gmail.com>
2026-04-06 12:12:56 +0500

    solishtirish

--- Day7/main.c
+++ Day7/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#define READ 1 // 0001
+#define WRITE 2 // 0010
+#define EXEC 4 // 0100
 
 int main(void) {
   int a = 5; // 0101
@@ -10,12 +13,33 @@ int main(void) {
   int x = 0; 
   x = x | 1; // 00000001 -> 1 (1 << 0)
   x = x | (1 << 1); // 00000010 -> 2
-  x = x | (1 << 2); // 00000100 -> 4
-  x = x | (1 << 3); // 00001000 -> 8
-  x = x | (1 << 4); // 00010000 -> 16
-  x = x | (1 << 5); // 00100000 -> 32
-  x = x | (1 << 6); // 01000000 -> 64
-  x = x | (1 << 7); // 10000000 -> 128
+  // x = x | (1 << 2); // 00000100 -> 4
+  // x = x | (1 << 3); // 00001000 -> 8
+  // x = x | (1 << 4); // 00010000 -> 16
+  // x = x | (1 << 5); // 00100000 -> 32
+  // x = x | (1 << 6); // 01000000 -> 64
+  // x = x | (1 << 7); // 10000000 -> 128
   printf("%d\n", x);
+
+  if (x & (1 << 1)) { // ozi xozir tepadan amallar tufayli x -> 00000011
+    // 00000011 shunaqa bolib kelayabdi. Men esa 1 - bit yoqilgan ekanini tekshiryabman
+    printf("Yoqilgan\n");
+  } else {
+    printf("O'chiq\n");
+  } // bu usul bilan faqat bitta bit tekshiriladi
+
+  int n = 10; // 00001010
+
+  if ((n & ((1 << 1) | (1 << 3))) == ((1 << 1) | (1 << 3))) {
+    printf("Ikkala bit ham yoqilgan\n");
+  }
+
+  int g = 86; // 01010110
+
+  int mask = (1 << 1) | (1 << 2) | (1 << 4) | (1 << 6);
+  if ((g & mask) == mask) {
+    printf("kerak hammasi bor\n");
+  }
+
   return 0;
 }
