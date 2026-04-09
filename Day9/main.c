84831ccf50990c2ce1aef0796b3c4feb5d50cdf7
Nurmuhammad <nursherzil@gmail.com>
2026-04-09 19:52:28 +0500

    short syntax masking

--- Day9/main.c
+++ Day9/main.c
@@ -20,5 +20,52 @@ int main(void) {
 
     a = a & ~(1 << 1); // 00000000
     printf("1 - bitni clear qilish = %d\n", a);
+
+    // short syntax
+    // TASK
+    // 1-bitni yoq
+    // 3-bitni yoq
+    // 1-bitni toggle qil
+    // 2-bitni yoq
+    // 3-bitni clear qil
+    int b = 0;
+
+    b |= (1 << 1); // 00000010
+    printf("short syntax, 1 - bit yoqish: %d\n", b);
+
+    b |= (1 << 3); // 00001010
+    printf("short syntax, 3 - bit yoqish: %d\n", b);
+
+    b ^= (1 << 1); // 00001000
+    printf("short syntax, 1 - bit toggle: %d\n", b);
+
+    b |= (1 << 2); // 00001100
+    printf("short syntax, 2 - bit yoqish: %d\n", b);
+
+    b &= ~(1 << 3); // 00000100
+    printf("short syntax, 3 - bit clear: %d\n", b);
+
+
+    // short syntax (task 2)
+    // 0, 2, 4-bitlarni yoq
+    // 2-bit toggle qil
+    // 5-bitni yoq
+    // 0 va 4-bitlarni clear qil
+    // faqat 5-bit qolishi kerak
+
+    int c = 0; // 00000000
+
+    c |= (1 << 0) | (1 << 2) | (1 << 4); // 00010101
+    printf("0, 2, 4-bitlar yoqildi: %d\n", c);
+
+    c ^= (1 << 2); // 00010001
+    printf("2-bit toggle qilindi: %d\n", c);
+    
+    c |= (1 << 5); // 00110001
+    printf("5-bit yoqildi: %d\n", c);
+
+    c &= ~((1 << 0) | (1 << 4)); // 00100000
+    printf("0 va 4-bitlar clear qilindi: %d\n", c);
+
     return 0;
 }
