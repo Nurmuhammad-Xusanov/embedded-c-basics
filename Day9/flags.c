0b2c595a8c0c0145f81e4ce3c3b1e47f32f3bf9a
Nurmuhammad <nursherzil@gmail.com>
2026-04-09 22:27:07 +0500

    upgraded flags

--- Day9/flags.c
+++ Day9/flags.c
@@ -18,26 +18,35 @@ int main (void) {
     perm |= READ | WRITE; // 00000011
     printf("Read va Write yoqildi: %d\n", perm);
 
-    //perm &= ~WRITE; // 00000001
-    //printf("Write o'chirildi: %d\n", perm);
+    perm &= ~WRITE; // 00000001
+    printf("Write o'chirildi: %d\n", perm);
+
+    //perm &= ~READ; // 00000000
+    //printf("Read o'cirildi: %d\n", perm);
 
     perm |= EXEC; // 00000101
     printf("Exec yoqildi: %d\n", perm);
 
     if(perm & READ) {
         printf("Read bor \n");
-    } else {
-        printf("Read yo'q \n");
+    }
+    if(!(perm & READ)) {
+        printf("Read yo'q\n");
     }
     if (perm & WRITE) {
         printf("Write bor \n");
-    } else {
-        printf("Write yoq \n");
+    } 
+    if(!(perm & WRITE)) {
+        printf("Write yo'q\n");
     }
 
     int mask = READ | WRITE;
     if ((perm & mask) == mask) {
-        printf("Read + Write bor");
+        printf("Read + Write bor\n");
+    }
+
+    if (perm == EXEC) {
+        printf("Faqat Exec bor\n");
     }
 
     return 0;
