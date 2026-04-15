c9bd12fb720e5d3c983af92b87cf7b7b04dc8e89
Nurmuhammad <nursherzil@gmail.com>
2026-04-15 22:37:34 +0500

    mods

--- Day12/main.c
+++ Day12/main.c
@@ -3,7 +3,17 @@
 int main(void) {
     int n;
     scanf("%d\n", &n);
+    
     int result = n & (-n);
     printf("%d\n", result);
+
+    int a;
+    scanf("%d\n", &a);
+    
+    while (a > 0) {
+        int bit = a & (-a);
+        printf("%d\n", bit);
+        a -= bit;
+    }
     return 0;
 }
